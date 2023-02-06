3fd32c0aa5c5ca8d59dae60103d404d613c0960d
Octavio Jara <octaviojara@MacBook-Pro-de-Octavio.local>
2023-02-06 02:06:48 -0300

    add post method

--- handler.js
+++ handler.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const querystring = require("querystring");
+
 module.exports.hello = async (event) => {
   return {
     statusCode: 200,
@@ -24,6 +26,24 @@ module.exports.helloUser = async (event) => {
       {
         message: `Hola ${event.pathParameters.name}`,
         input: event,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.createUser = async (event) => {
+  const user = querystring.parse(event["body"]);
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `Hola `,
+        input: `${user.name}`,
       },
       null,
       2
